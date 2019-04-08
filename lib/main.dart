3f790f386999209d92db6a57a7b85e6ffed7fe8e
邓启宋 <dengqs@physicalpoint.com>
2019-04-08 16:58:52 +0800

    资源使用与布局学习

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class RandomWordsState extends State<RandomWords> {
         title: new Text("Startup Name Generator"),
         centerTitle: true,
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaced)
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushLayout)
         ],
       ),
       body: _buildSuggestions(),
@@ -106,5 +106,104 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushLayout() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(builder: (context) => new LayoutWidget()));
+  }
 
+
+}
+
+class LayoutWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+          children: [new Expanded(child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[new Container(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: new Text("Oeschinen Lake Campground",
+                style: new TextStyle(
+                    fontWeight: FontWeight.bold
+                ),),
+            ),
+            new Text("Kandersteg, Switzerland",
+              style: new TextStyle(
+                  color: Colors.grey[500]
+              ),)
+            ],
+          )),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text("41")
+          ]
+      ),
+    );
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme
+          .of(context)
+          .primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+    return new Scaffold(
+        appBar: new AppBar(
+          title: Text("Flutter Layout"),
+          centerTitle: true,
+        ),
+      body: new ListView(
+        children: <Widget>[
+          new Image.asset(
+            "images/main_head.png",
+            width: 600.0,
+            height: 240.0,
+            fit: BoxFit.cover,
+          ),
+          titleSection,
+          buttonSection,
+          textSection
+        ],
+      ),
+    );
+  }
 }
